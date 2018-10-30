92c5642be3a8a2bd20bffc7956ca90949acb67ef
Chunlin Wang <chunlinwang1983@gmail.com>
2018-10-30 10:18:35 +0100

    add a new version

--- splitCsvHelp.sh
+++ splitCsvHelp.sh
@@ -17,3 +17,55 @@ do
     cat $file >> tmp_file
     mv -f tmp_file $file
 done
+
+# vervion 2
+#!/usr/bin/env bash
+
+# catch variables
+if [ -z $1 ]
+then
+    echo "Please specify the temporary dictionary"
+    exit 0
+else
+    tmpDir=$1
+fi
+
+if [ -z $2 ]
+then
+  echo "Please specify the csv file name"
+  exit 0
+else
+    fileFullName=$2
+fi
+
+if [ -z $3 ]
+then
+    lines=8000
+else
+    lines=$3
+fi
+
+path="${tmpDir}/${fileFullName}"
+
+cd $tmpDir
+
+IFS='.' read -r -a splitFileFullname <<< "$fileFullName"
+filePrefix=${splitFileFullname[0]}
+
+rm -rf $filePrefix
+mkdir $filePrefix && cd $filePrefix
+
+# set header of csv
+head -n 1 $path > header.csv
+
+# split file csv
+tail -n +2 $path | split -l $lines -d - "${filePrefix}_"
+
+# add header and rename csv file
+for file in ${filePrefix}_*
+do
+    cat header.csv $file > $file.csv;
+    rm $file;
+done
+
+rm header.csv
